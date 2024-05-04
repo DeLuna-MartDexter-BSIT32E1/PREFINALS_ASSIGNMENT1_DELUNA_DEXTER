764e1da79858e13aecdf412a784b67f03c665df5
STUDENT <STUDENT@DESKTOP-K1FFG82>
2024-05-04 09:10:59 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_DELUNA_DEXTER.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
